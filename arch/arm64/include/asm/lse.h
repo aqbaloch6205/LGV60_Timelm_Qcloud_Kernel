2e9c6874d7b81b7a888892030cbdda7c7afd3b3d
Will Deacon <will@kernel.org>
2019-08-29 11:22:30 +0100

    UPSTREAM: arm64: lse: Remove unused 'alt_lse' assembly macro
    The 'alt_lse' assembly macro has been unused since 7c8fc35dfc32
    ("locking/atomics/arm64: Replace our atomic/lock bitop implementations
    with asm-generic").
    
    Remove it.
    
    Reviewed-by: Andrew Murray <andrew.murray@arm.com>
    Change-Id: I1c8a1d30190fbb433f2bf36349ede0183598c5e2
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/include/asm/lse.h
+++ arch/arm64/include/asm/lse.h
@@ -12,40 +12,23 @@
 
 #ifdef __ASSEMBLER__
 
-.arch_extension	lse
-
-.macro alt_lse, llsc, lse
-	alternative_insn "\llsc", "\lse", ARM64_HAS_LSE_ATOMICS
-.endm
-
 #else	/* __ASSEMBLER__ */
 
 #ifdef CONFIG_LTO_CLANG
 #define __LSE_PREAMBLE	".arch_extension lse\n"
 #else
+
 __asm__(".arch_extension	lse");
 #define __LSE_PREAMBLE
 #endif
-
 
 /* In-line patching at runtime */
 #define ARM64_LSE_ATOMIC_INSN(llsc, lse)				\
 	ALTERNATIVE(llsc, __LSE_PREAMBLE lse, ARM64_HAS_LSE_ATOMICS)
 
-#endif	/* __ASSEMBLER__ */
 #else	/* CONFIG_AS_LSE && CONFIG_ARM64_LSE_ATOMICS */
-
-#ifdef __ASSEMBLER__
-
-.macro alt_lse, llsc, lse
-	\llsc
-.endm
-
-#else	/* __ASSEMBLER__ */
-
 
 #define ARM64_LSE_ATOMIC_INSN(llsc, lse)	llsc
 
-#endif	/* __ASSEMBLER__ */
 #endif	/* CONFIG_AS_LSE && CONFIG_ARM64_LSE_ATOMICS */
 #endif	/* __ASM_LSE_H */
