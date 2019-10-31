27d773d58ee8de4508910c17400a3e184df13658
Sami Tolvanen <samitolvanen@google.com>
2019-10-31 12:57:05 -0700

    UPSTREAM: arm64: lse: fix LSE atomics with LLVM's integrated assembler
    [ Upstream commit e0d5896bd356cd577f9710a02d7a474cdf58426b ]
    
    Unlike gcc, clang considers each inline assembly block to be independent
    and therefore, when using the integrated assembler for inline assembly,
    any preambles that enable features must be repeated in each block.
    
    This change defines __LSE_PREAMBLE and adds it to each inline assembly
    block that has LSE instructions, which allows them to be compiled also
    with clang's assembler.
    
    Link: https://github.com/ClangBuiltLinux/linux/issues/671
    Change-Id: I23955148f4a629f5a0216a2240bada710d37a00f
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Tested-by: Andrew Murray <andrew.murray@arm.com>
    Tested-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Andrew Murray <andrew.murray@arm.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/arm64/include/asm/lse.h
+++ arch/arm64/include/asm/lse.h
@@ -6,6 +6,8 @@
 
 #if defined(CONFIG_AS_LSE) && defined(CONFIG_ARM64_LSE_ATOMICS)
 
+#define __LSE_PREAMBLE	".arch armv8-a+lse\n"
+
 #include <linux/compiler_types.h>
 #include <linux/export.h>
 #include <linux/jump_label.h>
@@ -13,18 +15,6 @@
 #include <asm/alternative.h>
 #include <asm/atomic_lse.h>
 #include <asm/cpucaps.h>
-
-#ifdef __ASSEMBLER__
-
-#else	/* __ASSEMBLER__ */
-
-#ifdef CONFIG_LTO_CLANG
-#define __LSE_PREAMBLE	".arch_extension lse\n"
-#else
-
-__asm__(".arch_extension	lse");
-#define __LSE_PREAMBLE
-#endif
 
 extern struct static_key_false cpu_hwcap_keys[ARM64_NCAPS];
 extern struct static_key_false arm64_const_caps_ready;
