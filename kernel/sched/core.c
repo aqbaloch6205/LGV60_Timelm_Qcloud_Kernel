b24d1e52737891a38d6c7ded3e11504ece4340ee
UtsavBalar1231 <utsavbalar1231@gmail.com>
2024-06-22 10:47:49 +0530

    fix-miui-optimizations
    Change-Id: I4fa7cfddc5a8d3301a1157f9366f2b26d75180f7
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6467,7 +6467,6 @@ void show_state_filter(unsigned long state_filter)
 		debug_show_all_locks();
 }
 
-#ifdef CONFIG_XIAOMI_MIUI
 void show_state_filter_single(unsigned long state_filter)
 {
 	struct task_struct *g, *p;
@@ -6495,7 +6494,6 @@ void show_state_filter_single(unsigned long state_filter)
 	}
 	rcu_read_unlock();
 }
-#endif
 
 /**
  * init_idle - set up an idle thread for a given CPU
