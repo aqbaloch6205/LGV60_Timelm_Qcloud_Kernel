03a1b75240193d59f034051109f9e0a3f517f049
Karina Yankevich <k.yankevich@omp.ru>
2025-10-22 12:32:53 +0300

    ext4: xattr: fix null pointer deref in ext4_raw_inode()
    commit b97cb7d6a051aa6ebd57906df0e26e9e36c26d14 upstream.
    
    If ext4_get_inode_loc() fails (e.g. if it returns -EFSCORRUPTED),
    iloc.bh will remain set to NULL. Since ext4_xattr_inode_dec_ref_all()
    lacks error checking, this will lead to a null pointer dereference
    in ext4_raw_inode(), called right after ext4_get_inode_loc().
    
    Found by Linux Verification Center (linuxtesting.org) with SVACE.
    
    Fixes: c8e008b60492 ("ext4: ignore xattrs past end")
    Cc: stable@kernel.org
    Signed-off-by: Karina Yankevich <k.yankevich@omp.ru>
    Reviewed-by: Sergey Shtylyov <s.shtylyov@omp.ru>
    Reviewed-by: Baokun Li <libaokun1@huawei.com>
    Message-ID: <20251022093253.3546296-1-k.yankevich@omp.ru>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Ulrich Hecht <uli@kernel.org>

--- fs/ext4/xattr.c
+++ fs/ext4/xattr.c
@@ -1180,6 +1180,18 @@ ext4_xattr_inode_dec_ref_all(handle_t *handle, struct inode *parent,
 	unsigned int ea_ino;
 	int err;
 	int credits;
+	void *end;
+
+	if (block_csum)
+		end = (void *)bh->b_data + bh->b_size;
+	else {
+		err = ext4_get_inode_loc(parent, &iloc);
+		if (err) {
+			EXT4_ERROR_INODE(parent, "parent inode loc (error %d)", err);
+			return;
+		}
+		end = (void *)ext4_raw_inode(&iloc) + EXT4_SB(parent->i_sb)->s_inode_size;
+	}
 
 	/* One credit for dec ref on ea_inode, one for orphan list addition, */
 	credits = 2 + extra_credits;
