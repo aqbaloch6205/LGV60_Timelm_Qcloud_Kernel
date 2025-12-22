417e3899787123cf8a1177ccd6f08803e88a70d5
aqbaloch6205 <aqbaloch6205@gmail.com>
2025-12-22 23:21:18 +0500

    Update dtbbuild.sh

--- dtbbuild.sh
+++ dtbbuild.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 TOOLCHAIN_PATH="$HOME/zyc-clang/bin"
 OUT_DIR="out"
-DEFCONFIG="vendor/jhatpat_defconfig"
+DEFCONFIG="vendor/arabella_defconfig"
 
 # Check toolchain
 if [ ! -d "$TOOLCHAIN_PATH" ]; then
