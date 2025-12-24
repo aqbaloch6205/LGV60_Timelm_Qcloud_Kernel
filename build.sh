793c58af7bccaa0982783a21bc7661db235a1c1f
aqbaloch6205 <aqbaloch6205@gmail.com>
2025-12-24 15:02:09 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -e
 
 # Toolchain and identity
 TOOLCHAIN_PATH=$HOME/zyc-clang/bin
-export KBUILD_BUILD_USER="Bhazheng"
-export KBUILD_BUILD_HOST="cachyos"
+export KBUILD_BUILD_USER="QCloudfx"
+export KBUILD_BUILD_HOST="Abdul-Qadeer"
 export ARCH=arm64
 export SUBARCH=arm64
 export PATH="$TOOLCHAIN_PATH:$PATH"
