c7d20dd99bc80f8b77a4060cc1a510a3d3be6fc7
aqbaloch6205 <aqba6205@gmail.com>
2026-01-31 15:28:25 +0000

    non gki

--- kernel.sh
+++ kernel.sh
@@ -55,7 +55,7 @@ make "${MAKE_ARGS[@]}" $DEFCONFIG
 if [ $KSU_ENABLE -eq 1 ]; then
     echo "KSU is enabled"
     # Exact Xiaomi dev trick: pulling the remote setup logic
-    curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s main
+     curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
     
     echo "Applying SuSFS v2.0.0 Configs..."
     ./scripts/config --file $OUT_DIR/.config \
